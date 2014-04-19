f9ae8f3e22f7b6948df26d70caa809f3fe0661c2
Peter Tomozawa <peter@tomozawa.us>
2014-04-19 02:17:54 +0000

    Post database additions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
+  resources :posts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,3 +55,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
