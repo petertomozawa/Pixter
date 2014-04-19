5f61bac2764fb514f5139dab4fc723a26d28e961
Peter Tomozawa <peter@tomozawa.us>
2014-04-19 22:11:04 +0000

    new pages

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 gem "twitter-bootstrap-rails"
+gem 'simple_form'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,20 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def create
+    Post.create( post_params )
+    redirect_to posts_path
+  end
+  def new
+    @post = Post.new
+  end  
+private
+    
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
+  
+  
 end
+
 
